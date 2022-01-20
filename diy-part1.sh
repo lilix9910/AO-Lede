fe4b20b8215cc59bc5a9cafa1f5800ee76627e47
lilix9910 <98016077+lilix9910@users.noreply.github.com>
2022-01-21 01:45:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/mtk
