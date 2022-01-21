bd2fee61cd06def573889499c3cd9bba65619f04
lilix9910 <98016077+lilix9910@users.noreply.github.com>
2022-01-21 10:35:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 
 #git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/mtk
+#git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/mtk
